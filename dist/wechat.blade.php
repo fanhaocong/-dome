d4209919793b128180356d9dc804c45978d638ac
fanhaocong <45645143+fanhaocong@users.noreply.github.com>
2019-01-04 14:30:23 +0800

    Add files via upload

---
+++ dist/wechat.blade.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="x5-orientation"content="portrait">
+  <meta name="last-build-time" content="Fri Jan 04 2019 13:39:05 GMT+0800 (GMT+08:00)">
+  <!--link rel="apple-touch-icon" href="apple-touch-icon.png"-->
+  <script>
+    var designWidth = 375;
+    var screenWidth = screen.width;
+    var scale = screenWidth / designWidth;
+    var meta = '<meta name="viewport" content="width=' + designWidth + ", initial-scale=" + scale + ", maximum-scale=" + scale + ", minimum-scale=" + scale + ', user-scalable=no, target-densitydpi=device-dpi, minimal-ui" >';
+    document.write(meta);
+
+    // 调试工具，url 中加入 debug 开启
+    ;(function () {
+        if (!/debug/.test(window.location)) return;
+        var src = '/lib/eruda.min.js';
+        document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
+        document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
+    })();
+  </script>
+  <script src="/lib/jweixin-1.0.0.js"></script>
+  <script>
+    // 微信 config，类型可以是 JS Object 或者 JSON 字符串
+    window.wxConfig = {!! $jsConfig !!}; // 或者 '{}'
+
+
+    window.apiHost='{!! $apiHost !!}';
+
+    // server 端下发 token 以及 uniqueKey
+    window.token = '{{$token}}';
+
+    //server 下发的uniqueKey
+    window.uniqueKey = '{{$uniqueKey}}';
+
+    // server 下发的服务对象,可用于过滤等
+    window.serviceTarget = {!! $serviceTarget !!};
+
+    // server 下发的服务类型,可用于过滤等
+    window.serviceCategory = {!! $serviceCategory !!};
+
+     // server 下发的服务类型,可用于过滤等
+     window.teamCategory = {!! $teamCategory !!};
+
+    // server 下发的个人擅长, 用于用户资料填写
+    window.goodAt = {!! $gootAt !!};
+
+    // server 下发的机构信息
+    window.orgInfo = {!! $orgInfo !!};
+
+    // server 下发的机构编号
+    window.orgCode = '{!! $orgCode !!}';
+
+    if (window.wxConfig) {
+      try{
+        if (typeof window.wxConfig === 'string') {
+          window.wxConfig = JSON.parse(window.wxConfig);
+        }
+        window.wx.config(wxConfig);
+      }
+      catch(e) {
+        alert(e);
+      }
+    }
+
+    window.orgInfo && (document.title = window.orgInfo.name);
+  </script>
+  <script>
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?a09aed27d797ec85ef89a5f15c0d32a1";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+  </script>
+
+
+</head>
+
+<body>
+  <div id="container">
+  </div>
+  <script charset="utf-8" src="http://map.qq.com/api/js?v=2.exp"></script>
+  <script src="/dist/main.5e69cde449cb32786995.js"></script>
+</body>
+
+</html>
